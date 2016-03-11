ef665fde7ee9052050e378c11095aec9c554c33c
Daniel <nguyen.leenguyen@gmail.com>
2016-03-11 11:20:07 +0700

    Product Admin Control part 2

---
+++ src/main/java/com/emusicstore/controller/admin/AdminController.java
@@ -0,0 +1,38 @@
+package com.emusicstore.controller.admin;
+
+import com.emusicstore.model.Customer;
+import com.emusicstore.model.Product;
+import com.emusicstore.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/admin")
+public class AdminController {
+
+    @Autowired
+    private ProductService productService;
+
+    @RequestMapping
+    private String adminPage() {
+        return "admin";
+    }
+
+    @RequestMapping(value = "/productInventory")
+    public String productInventory(Model model) {
+        List<Product> products = productService.getProductList();
+        model.addAttribute("products", products);
+        return "productInventory";
+    }
+
+    @RequestMapping(value = "/customer")
+    public String customerManagement(Model model) {
+
+        return "customerManagement";
+    }
+
+}
--- src/main/java/com/emusicstore/model/Users.java
+++ src/main/java/com/emusicstore/model/Users.java
@@ -7,7 +7,6 @@ import javax.persistence.Id;
 @Entity
 public class Users {
 
-
     @Id
     @GeneratedValue
     private int usersId;
